394e046eab8f9040d348abefb2543ed7b57114ea
Alexey Lapitksy <lex@realisticgroup.com>
2022-01-24 22:05:48 +0100

    fix tabs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import './Navbar.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import '../pages/About';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import AppBar from '@mui/material/AppBar';
+import React, { useState } from "react";
+import "./Navbar.scss";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "../pages/About";
+import Tab from "@mui/material/Tab";
+import Tabs from "@mui/material/Tabs";
+import AppBar from "@mui/material/AppBar";
 
 function Navbar() {
   // const { match } = props;
@@ -33,20 +33,23 @@ function Navbar() {
   //   setSelectedTab(tab);
   // };
 
+  const [state, setState] = useState(0);
+
+  let handleChange = (event: any, value: any) => {
+    setState(value as number);
+  };
+
   return (
-    <Tabs centered>
-      <Link to="/">
-        <Tab label="Startsida" />
-      </Link>
-      <Link to="Reviews">
-        <Tab label="Recensioner" />
-      </Link>
-      <Link to="map">
-        <Tab label="SemmelKarta" />
-      </Link>
-      <Link to="About">
-        <Tab label="Om" />
-      </Link>
+    <Tabs
+      centered
+      value={state}
+      onChange={handleChange}
+      indicatorColor={"secondary"}
+    >
+      <Tab label="Startsida" to="/" component={Link} />
+      <Tab label="Recensioner" to="reviews" component={Link} />
+      <Tab label="SemmelKarta" to="map" component={Link} />
+      <Tab label="Om" to="about" component={Link} />
     </Tabs>
   );
 }
